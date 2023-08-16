52296cb8b059ea11f1062991771cbf6bda1faf7f
amigin <amigin@gmail.com>
2023-08-16 12:08:00 +0300

    Fixed MacrosName

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use proc_macro::TokenStream;
 mod with_telemetry;
 
 #[proc_macro_attribute]
-pub fn generate_grpc_client(attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn with_telemetry(attr: TokenStream, item: TokenStream) -> TokenStream {
     match crate::with_telemetry::generate(attr, item) {
         Ok(result) => result,
         Err(err) => err.into_compile_error().into(),
